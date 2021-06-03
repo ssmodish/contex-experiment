b99f7b5e7f8d8953f1aed7eb3f901e88090113b3
Steve Smodish <smodish@hotmail.com>
2021-06-03 14:25:58 -0400

    Changed demo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-function App() {
-  return (
-    <div className='App'>
-      <h1>hello</h1>
-    </div>
-  )
+import React, { Component } from 'react'
+import { CardList } from './components/card-list/card-list.component'
+
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      monsters: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }))
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <CardList monsters={this.state.monsters} />
+      </div>
+    )
+  }
 }
 
 export default App
